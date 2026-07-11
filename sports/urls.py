6bef89f153ef623fb2a58eee27d7252f2f912897
Tomasz_Malicki <123742@student.san.edu.pl>
2026-07-11 18:17:08 +0200

    update urls

--- sports/urls.py
+++ sports/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MatchListView, MatchDetailView
+from .views import MatchListView, MatchDetailView, trigger_fetch
 
 urlpatterns = [
     path('matches/', MatchListView.as_view(), name='match-list'),
     path('matches/<int:pk>/', MatchDetailView.as_view(), name='match-detail'), 
+    
+    # NOWA ŚCIEŻKA DO AKTUALIZACJI MECZÓW
+    path('trigger-fetch/', trigger_fetch, name='trigger_fetch'), 
 ]
