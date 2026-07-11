eb5d21670b40c60354524ab0f98614a9caed69ba
Tomasz_Malicki <123742@student.san.edu.pl>
2026-07-11 23:32:40 +0200

    Update serializers.py

--- sports/serializers.py
+++ sports/serializers.py
@@ -13,14 +13,15 @@ class AnalysisSerializer(serializers.ModelSerializer):
 
 class MatchSerializer(serializers.ModelSerializer):
     videos = VideoSerializer(many=True, read_only=True)
-    # ZMIANA: Zmieniamy sposób ładowania analiz na niestandardowy
+    # 1. ZMIENIONA LINIJKA:
     analyses = serializers.SerializerMethodField() 
 
     class Meta:
         model = Match
         fields = ['id', 'home_team', 'away_team', 'match_date', 'home_logo', 'away_logo', 'videos', 'analyses']
 
-    # NOWA FUNKCJA: Filtruje analizy przed wysłaniem na stronę
+    # 2. NOWA FUNKCJA NA SAMYM DOLE (pamiętaj o wcięciach!):
     def get_analyses(self, obj):
+        # Pobieramy tylko te, które mają zaznaczone is_approved
         approved_analyses = obj.analyses.filter(is_approved=True)
         return AnalysisSerializer(approved_analyses, many=True).data
