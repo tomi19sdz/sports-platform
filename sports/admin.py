4cbdd6bde4884eae68136ec4905be11cbdf5da02
Tomasz_Malicki <123742@student.san.edu.pl>
2026-07-19 16:52:36 +0200

    Update admin.py

--- sports/admin.py
+++ sports/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Match, Analysis, ChatMessage, Video
-from .utils import aktualizuj_analize  # Importujemy funkcję dopisującą dane
-
-# Rejestracja wideo
-admin.site.register(Video)
+from .utils import aktualizuj_analize  # Upewnij się, że funkcja jest w utils.py
 
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
@@ -16,22 +13,19 @@ class AnalysisAdmin(admin.ModelAdmin):
     list_filter = ('is_approved', 'created_at')
     actions = ['approve_analyses', 'update_analysis_ai']
 
-    # Twoja istniejąca akcja
     @admin.action(description="Zatwierdź wybrane analizy")
     def approve_analyses(self, request, queryset):
         queryset.update(is_approved=True)
 
-    # Nowa akcja do aktualizacji przez AI
     @admin.action(description="Aktualizuj analizę AI (dopisz nowe dane)")
     def update_analysis_ai(self, request, queryset):
         for obj in queryset:
-            # Zakładam, że Twoje pole z tekstem analizy w modelu Analysis nazywa się 'text' lub 'content'
-            # Zmień 'text' na nazwę pola, w którym przechowujesz treść analizy
-            stara_tresc = obj.text 
+            # Używamy pola 'content' tak jak w Twoim modelu
+            stara_tresc = obj.content 
             mecz_nazwa = f"{obj.match.home_team} vs {obj.match.away_team}"
             
             # Wywołujemy funkcję aktualizującą
-            obj.text = aktualizuj_analize(stara_tresc, mecz_nazwa)
+            obj.content = aktualizuj_analize(stara_tresc, mecz_nazwa)
             obj.save()
 
 @admin.register(ChatMessage)
@@ -39,3 +33,5 @@ class ChatMessageAdmin(admin.ModelAdmin):
     list_display = ('author', 'match', 'text', 'created_at')
     list_filter = ('match', 'created_at')
     search_fields = ('author', 'text')
+
+admin.site.register(Video)
