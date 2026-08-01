fb022a8e2386d02fdcd490e965b1762903bb8212
Tomasz_Malicki <123742@student.san.edu.pl>
2026-08-01 22:23:34 +0200

    Update admin.py

--- sports/admin.py
+++ sports/admin.py
@@ -1,36 +1,56 @@
 from django.contrib import admin
-from .models import Match, Analysis, ChatMessage, Video
-from .utils import aktualizuj_analize
+from django import forms
+from django.forms import widgets
+from django.utils.safestring import mark_safe
+from .models import Match, Analysis, ChatMessage
 
+# 1. Tworzymy specjalny widget (hybrydę pola tekstowego i listy)
+class DatalistWidget(widgets.TextInput):
+    def __init__(self, data_list, name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._name = name
+        self._list = data_list
+        # Dodajemy atrybut 'list' do pola <input>
+        self.attrs.update({'list': f'list__{self._name}'})
+
+    def render(self, name, value, attrs=None, renderer=None):
+        text_html = super().render(name, value, attrs=attrs, renderer=renderer)
+        # Budujemy ukrytą listę podpowiedzi <datalist>
+        data_html = f'<datalist id="list__{self._name}">'
+        for item in self._list:
+            data_html += f'<option value="{item}">'
+        data_html += '</datalist>'
+        return mark_safe(text_html + data_html)
+
+
+# 2. Tworzymy niestandardowy formularz dla panelu admina
+class MatchAdminForm(forms.ModelForm):
+    class Meta:
+        model = Match
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Pobieramy wszystkie unikalne nazwy lig, które JUŻ SĄ w bazie (i nie są puste)
+        leagues = Match.objects.exclude(league__isnull=True).exclude(league__exact='').values_list('league', flat=True).distinct()
+        
+        # Podpinamy nasz widget do pola 'league'
+        if 'league' in self.fields:
+            self.fields['league'].widget = DatalistWidget(data_list=leagues, name='league')
+            self.fields['league'].help_text = "Wybierz z listy lub wpisz zupełnie nową ligę."
+
+
+# 3. Rejestrujemy model Match z nowym formularzem
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    list_display = ('home_team', 'away_team', 'league', 'match_date', 'prediction_status')
-    list_editable = ('prediction_status',) # Pozwala zmieniać status bezpośrednio na liście
+    form = MatchAdminForm
+    
+    # Opcjonalnie: ułatwia przeglądanie meczów w tabeli w panelu admina
+    list_display = ('home_team', 'away_team', 'league', 'match_date', 'status')
+    list_filter = ('league', 'status')
     search_fields = ('home_team', 'away_team', 'league')
-    fields = ('home_team', 'away_team', 'league', 'home_logo', 'away_logo', 'match_date', 'home_score', 'away_score', 'status', 'prediction_status')
 
-@admin.register(Analysis)
-class AnalysisAdmin(admin.ModelAdmin):
-    list_display = ('match', 'created_at', 'is_approved')
-    list_filter = ('is_approved', 'created_at')
-    actions = ['approve_analyses', 'update_analysis_ai']
-
-    @admin.action(description="Zatwierdź wybrane analizy")
-    def approve_analyses(self, request, queryset):
-        queryset.update(is_approved=True)
-
-    @admin.action(description="Aktualizuj analizę AI (dopisz nowe dane)")
-    def update_analysis_ai(self, request, queryset):
-        for obj in queryset:
-            stara_tresc = obj.content 
-            mecz_nazwa = f"{obj.match.home_team} vs {obj.match.away_team} w {obj.match.league}"
-            obj.content = aktualizuj_analize(stara_tresc, mecz_nazwa)
-            obj.save()
-
-@admin.register(ChatMessage)
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_display = ('author', 'match', 'text', 'created_at')
-    list_filter = ('match', 'created_at')
-    search_fields = ('author', 'text')
-
-admin.site.register(Video)
+# Jeśli masz już zarejestrowane inne modele, zostaw je poniżej, np:
+# admin.site.register(Analysis)
+# admin.site.register(ChatMessage)
